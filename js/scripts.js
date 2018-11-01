f57e4f73a9bbe12db911ddd172f664a1bcb170c6
Student <example@example.com>
2018-11-01 13:19:51 -0700

    make some changes in the code to show which player has the control to play

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function Players() {
 
 Players.prototype.addPlayer = function(player) {
   player.Id = this.playerID();
+  console.log(player.Id);
   this.players.push(player);
 }
 
@@ -15,19 +16,19 @@ Players.prototype.playerID = function() {
 }
 
 
-//Bussiness Logic for Player
+//Bussiness Logic for Game
 function Game() {
   this.roll = 0,
   this.temp = 0,
   this.total = 0,
-  this.turn = 0
+  this.turn
 }
 
 Game.prototype.NewGame = function() {
   this.roll = 0;
   this.temp = 0;
   this.total = 0;
-  this.turn = 0;
+  //this.turn = 0;
 }
 
 Game.prototype.CheckForHundred = function(){
@@ -36,53 +37,56 @@ Game.prototype.CheckForHundred = function(){
   }
 }
 
-Game.prototype.SwitchTo = function() {
-  this.turn = "it's your turn";
-}
-
-Game.prototype.SwitchOff = function() {
-  this.turn = "it's NOT your turn";
-}
-
-Game.prototype.WhosTurn = function(player) {
-  if (this.turn === 1) {
-    return "It's your turn!"
-  }
-  else if (this.turn === 0) {
-    return "It's NOT your turn!"
-  }
-}
-
-Game.prototype.Roll = function(Roll) {
+Game.prototype.Roll1 = function(Roll) {
   if (Roll === 1) {
     this.roll = 0;
     this.temp = 0;
     this.turn = 0;
-    // this.turn = "it's NOT your turn";
-    alert("it's NOT your turn");
-  }
-  else if (Roll > 1 && Roll <= 6) {
+    $("#turnOne").text("It's NOT your turn");
+    $("#turnTwo").text("It's your turn");
+  }else if (Roll > 1 && Roll <= 6) {
     this.roll = Roll;
-    // this.turn = "it's Your turn";
+    $("#turnOne").text("It's your turn");
+    $("#turnTwo").text("It's NOT your turn");
   }
   return this.roll;
   return this.turn;
+}
 
+Game.prototype.Roll2 = function(Roll) {
+  if (Roll === 1) {
+    this.roll = 0;
+    this.temp = 0;
+    this.turn = 0;
+    $("#turnTwo").text("It's NOT your turn");
+    $("#turnOne").text("It's your turn");
+  }else if (Roll > 1 && Roll <= 6) {
+    this.roll = Roll;
+      $("#turnTwo").text("It's your turn");
+      $("#turnOne").text("It's NOT your turn");
+  }
+  return this.roll;
+  return this.turn;
 }
 
 Game.prototype.Temp = function() {
-  // if (this.roll > 1 && this.roll <= 6) {
     this.temp += this.roll;
-  // }
-  // else if (this.roll === 1) {
-  //   this.temp = 0;
-  // }
 }
 
-Game.prototype.Hold = function() {
+Game.prototype.Hold1 = function() {
   this.total += this.temp;
   this.roll = 0;
   this.temp = 0;
+  $("#turnOne").text("It's NOT your turn");
+  $("#turnTwo").text("It's your turn");
+}
+
+Game.prototype.Hold2 = function() {
+  this.total += this.temp;
+  this.roll = 0;
+  this.temp = 0;
+  $("#turnOne").text("It's your turn");
+  $("#turnTwo").text("It's NOT your turn");
 }
 
 function RollDice() {
@@ -103,91 +107,50 @@ var players = new Players;
 var Player1 = new Game;
 var Player2 = new Game;
 
-var turnSwitch1 = function(roll) {
-  if (roll === 1) {
-    Player2.turn = 1;
-  }
-    else {
-    Player2.turn = 0;
-    }
-
-  return Player2.turn;
-}
-
-//User Interface Logi
+//User Interface Logic
 $(document).ready(function() {
-  // $("#newPlayer").submit(function(event) {
-  //   event.preventDefault();
-  //   var inputtedPlayerName = $("input#newName").val();
-  //   $("input#newName").val("");
-  //   var newPlayer = new Game(inputtedPlayerName);
-  //   players.addPlayer(newPlayer);
-  //   displayPlayerDetails(players);
-  // });
-  //
-  // $("#rollButton").click(function(event) {
-  //   Game.Roll(RollDice());
-  //   Game.Temp();
-  //   displayPlayerDetails(players);
-  //
-  // });
+  $("#newPlayer").submit(function(event) {
+    event.preventDefault();
+    var inputtedPlayerName = $("input#newName").val();
+    $("input#newName").val("");
+    var newPlayer = new Game(inputtedPlayerName);
+    players.addPlayer(newPlayer);
+    displayPlayerDetails(players);
+  });
 
   $("#PlayerOneRoll").click(function(event) {
-    // $("#turnOne").text("It's Your Turn");
-    // $("#turnTwo").text("It's Not Your Turn");
-    // $("#turnOne").text("It's Your Turn");
-    // $("#turnTwo").text("It's Not Your Turn");
-    Player1.Roll(RollDice());
+    Player1.Roll1(RollDice());
     Player1.Temp();
     $("#diceRoll").html("<img src=img/" + Player1.roll + ".png>");
     $("#currentRollOne").text(Player1.roll);
     $("#turnTotalOne").text(Player1.temp);
-    turnSwitch1(Player1.roll);
-
-
-
-    // $("#turnOne").text(Player1.turn);
-    $("#turnTwo").text(Player2.turn);
-
- });
+    //$("#turnTwo").text(Player2.turn);
+  });
 
   $("#PlayerOneHold").click(function(event) {
-    Player1.Hold();
-      // alert("It's Now Player2's Turn");
-    // Player1.SwitchOff();
-    // $("#turnOne").text(Player1.turn);
-    // Player2.SwitchTo();
-    // $("#turnTwo").text(Player2.turn);
+    Player1.Hold1();
+
     $("#totalScoreOne").text(Player1.total);
     Player1.CheckForHundred();
      $("#playerOneScore").text(Player1.total);
-     // $("#turnOne").text("It's Not Your Turn");
-     // $("#turnTwo").text("It's Your Turn");
    });
 
    $("#PlayerTwoRoll").click(function(event) {
-     $("#turnOne").text("It's Not Your Turn");
-     $("#turnTwo").text("It's Your Turn");
-    Player2.Roll(RollDice());
+     // $("#turnOne").text("It's Not Your Turn");
+     // $("#turnTwo").text("It's Your Turn");
+    Player2.Roll2(RollDice());
     $("#diceRoll").html("<img src=img/" + Player2.roll + ".png>");
     Player2.Temp();
     $("#currentRollTwo").text(Player2.roll);
     $("#turnTotalTwo").text(Player2.temp);
-    // $("#turnTwo").text(Player2.turn);
   });
 
   $("#PlayerTwoHold").click(function(event) {
-    Player2.Hold();
-    // alert("It's Now Player1's Turn");
+    Player2.Hold2();
     $("#totalScoreTwo").text(Player2.total);
     Player2.CheckForHundred();
     $("#playerTwoScore").text(Player2.total);
-    // Player2.SwitchOff();
-    // $("#turnTwo").text(Player2.turn);
-    // Player1.SwitchTo();
-    // $("#turnOne").text(Player1.turn);
-    // $("#turnOne").text("It's Your Turn");
-    // $("#turnTwo").text("It's Not Your Turn");
+
   });
 
   $("#startGame").click(function(event) {
@@ -196,19 +159,12 @@ $(document).ready(function() {
     $("#currentRollOne").text("");
     $("#turnTotalOne").text("");
     $("#totalScoreOne").text("");
-
     $("#currentRollTwo").text("");
     $("#turnTotalTwo").text("");
     $("#totalScoreTwo").text("");
-
     $("#playerOneScore").text("");
     $("#playerTwoScore").text("");
-
-    Player1.turn = 1;
-    Player2.turn = 0;
-    // WhosTurn(Player1);
     $("#turnOne").text(Player1.turn);
-    // WhosTurn(Player2);
     $("#turnTwo").text(Player2.turn);
   })
 })
